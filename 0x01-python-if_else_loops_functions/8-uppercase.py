03b3ee5b4ed9ace20c1dd14b75982403a2d8517d
CletosMarcos <cletosmarcos@gmail.com>
2024-03-19 18:28:22 -0700

    add task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in range(len(str)):
+        if (ord(str[i]) in range(97,123)):
+            print("{}".format(chr(ord(str[i])-32) if i< len(str)-1 else (chr(ord(str[i])-32))+"\n"), end="")
+
+        else:
+            print("{}".format(str[i]), end="")
