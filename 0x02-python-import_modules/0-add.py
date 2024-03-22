9c1940760d0c9030b66b26cb4ca2ba90f227ba3b
CletosMarcos <cletosmarcos@gmail.com>
2024-03-21 19:30:20 -0700

    review task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-f_add = __import__('add_0').add
+if __name__ == "__main__":
 
-f_add.a = 1
-f_add.b = 2
+    f_add = __import__('add_0').add
 
-print("{} + {} = {}".format(f_add.a, f_add.b, f_add(f_add.a, f_add.b)))
+    f_add.a = 1
+    f_add.b = 2
+
+    print("{} + {} = {}".format(f_add.a, f_add.b, f_add(f_add.a, f_add.b)))
