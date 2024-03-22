d50975a9b2e76cce90cc0b817e89790d5219c0f7
CletosMarcos <cletosmarcos@gmail.com>
2024-03-21 19:49:43 -0700

    review solution

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-f_add = __import__('add_0').add
+import add_0 as add
 
 if __name__ == "__main__":
-    f_add.a = 1
-    f_add.b = 2
+    a = 1
+    b = 2
 
-    print("{} + {} = {}".format(f_add.a, f_add.b, f_add(f_add.a, f_add.b)))
+    print("{} + {} = {}".format(a, b, add.add(a, b)))
