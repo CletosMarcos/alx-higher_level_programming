e575ad94df5d78f3dcc59210eb25913bbfbe79eb
CletosMarcos <cletosmarcos@gmail.com>
2024-03-25 19:37:52 -0700

    review task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
+from sys import argv
+
 if __name__ == "__main__":
 
-    if len(argv) == 1:
+    if len(argv) - 1 == 1:
         print("1 argument:")
-    elif len(argv) == 0:
+    elif len(argv) - 1 == 0:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(len(argv)))
+        print("{} arguments:".format(len(argv) - 1))
 
-
-    for i in range(len(argv)):
-        print("{}: {}".format(i + 1, argv[i]))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
