b0a34f64778802d7620eeb1b2c249a87e0ea2e80
CletosMarcos <cletosmarcos@gmail.com>
2024-03-26 18:30:36 -0700

    add task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, div, mul
+from sys import argv
+
+if __name__ == "__main__":
+    print(len(argv))
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    elif argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    elif argv[2] == '+':
+        print("{} + {} = {}".format(int(argv[1]), int(argv[3]), add(int(argv[1]), int(argv[3]))))
+        exit(0)
+    
+    elif argv[2] == '-':
+        print("{} - {} = {}".format(int(argv[1]), int(argv[3]), sub(int(argv[1]), int(argv[3]))))
+        exit(0)
+
+    elif argv[2] == '*':
+        print("{} * {} = {}".format(int(argv[1]), int(argv[3]), mul(int(argv[1]), int(argv[3]))))
+        exit(0)
+
+    elif argv[2] == '/':
+        print("{} / {} = {}".format(int(argv[1]), int(argv[3]), div(int(argv[1]), int(argv[3]))))
+        exit(0)
+
+
