ba1f3474ee70e6d45ca4f5fcaa520c34226af409
CletosMarcos <cletosmarcos@gmail.com>
2024-04-01 20:47:10 -0700

    add task

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        max_nr = 0
+
+        for nr in my_list:
+            if nr > max_nr:
+                max_nr = nr
+
+        return max_nr
