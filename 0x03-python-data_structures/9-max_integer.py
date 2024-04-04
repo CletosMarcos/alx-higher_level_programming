162ab1f6d42ff92a8c58b95f062c6c71b60e24aa
CletosMarcos <cletosmarcos@gmail.com>
2024-04-03 18:55:34 -0700

    change the initial compared value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,10 +4,10 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        max_nr = 0
+        max_nr = my_list[0]
 
         for nr in my_list:
             if nr > max_nr:
                 max_nr = nr
 
-        return int(max_nr)
+        return max_nr
