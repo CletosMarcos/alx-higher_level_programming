18b0667432f1c528e9fcd51648ac737902387ad6
CletosMarcos <cletosmarcos@gmail.com>
2024-04-09 15:59:04 -0700

    add task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+
+    else:
+        roman_letters = [ ["I", 1], ["V", 5], ["X", 10],
+                          ["L", 50], ["C", 100], ["D", 500], ["M", 1000] ]
+
+        roman_sum = 0
+        last_num = 0
+
+        for letter in roman_string:
+            for elem in roman_letters:
+                if letter == elem[0]:
+                    if last_num == 0 or last_num >= elem[1]:
+                        roman_sum += elem[1]
+                    else: # if last_num < elem[1]
+                        roman_sum += elem[1] - (last_num * 2)
+                    last_num = elem[1]
+
+    return roman_sum
