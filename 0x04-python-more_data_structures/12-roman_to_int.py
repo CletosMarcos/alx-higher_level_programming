5c63e3104e1b57ca1cf24732eae8d65127b7101e
CletosMarcos <cletosmarcos@gmail.com>
2024-04-09 16:02:38 -0700

    review pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,8 @@ def roman_to_int(roman_string):
         return 0
 
     else:
-        roman_letters = [ ["I", 1], ["V", 5], ["X", 10],
-                          ["L", 50], ["C", 100], ["D", 500], ["M", 1000] ]
+        roman_letters = [["I", 1], ["V", 5], ["X", 10],
+                         ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]
 
         roman_sum = 0
         last_num = 0
@@ -16,7 +16,7 @@ def roman_to_int(roman_string):
                 if letter == elem[0]:
                     if last_num == 0 or last_num >= elem[1]:
                         roman_sum += elem[1]
-                    else: # if last_num < elem[1]
+                    else:  # elif last_num < elem[1]
                         roman_sum += elem[1] - (last_num * 2)
                     last_num = elem[1]
 
