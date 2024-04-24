1557c678c5e9ba2d764ce22f5a9463e0d4114e9f
CletosMarcos <cletosmarcos@gmail.com>
2024-04-23 19:28:05 -0700

    add task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" Defines a class Square."""
+
+
+class Square:
+    """ Initializes the square."""
+    def __init__(self, size=0):
+        self.__size = size
+
+    """ size getter method."""
+    @property
+    def size(self):
+        return self.__size
+
+    """ size setter."""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """ Determines the current area."""
+    def area(self):
+        return self.__size ** 2
