1c0b3121d40ed172e41bdf95ee41d8bcf22b088f
CletosMarcos <cletosmarcos@gmail.com>
2024-05-10 19:31:40 -0700

    add task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+module containing an ADD function
+"""
+
+def add_integer(a, b=98):
+    """ computes the addition of a and b
+
+        Args: a and b must be integer or float
+              if not must raise TypeError Exception
+        Return: an integer a + b
+    """
+    typevar = [int, float]
+
+    if type(a) not in typevar:
+        raise TypeError("a must be an integer")
+    if type(b) not in typevar:
+        raise TypeError("b must be an integer")
+
+    return int(a + b)
