ad0c3ae0cd1bbb13db0c0703c837310ca5dda53e
CletosMarcos <cletosmarcos@gmail.com>
2024-05-12 20:20:13 -0700

    add task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Prints a square"""
+
+
+def print_square(size):
+    """
+    Prints a square of size "size"
+
+    args:
+        size: size of the square
+
+        size must be an integer and >= 0 otherwise,
+        raise a ValueError
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
