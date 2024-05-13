8c5a95a6d4e736a2a8e01f23e89a394632b1040a
CletosMarcos <cletosmarcos@gmail.com>
2024-05-12 19:42:27 -0700

    review task 3 error handling for none letters only names

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,6 +2,7 @@
 
 """A module to print the full name"""
 
+
 def say_my_name(first_name, last_name=""):
     """
     Prints the full name if name is a string,
@@ -15,21 +16,13 @@ def say_my_name(first_name, last_name=""):
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
 
-    """
-    else:
-        for i in first_name:
-            if type(i) is not str:
-                raise TypeError("first_name must only contain strings")
-    """
+    if not all(char.isalpha() or char.isspace() for char in first_name):
+        raise TypeError("first_name must only contain letters")
 
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    """
-    else:
-        for i in last_name:
-            if type(i) is not str:
-                raise TypeError("last_name must only contain strings")
-    """
+    if not all(char.isalpha() or char.isspace() for char in last_name):
+        raise TypeError("last_name must only contain letters")
 
     print(f"{first_name} {last_name}")
