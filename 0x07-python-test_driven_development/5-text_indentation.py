1eefa524afea748cce24770195917dd10c84d3f4
CletosMarcos <cletosmarcos@gmail.com>
2024-05-13 20:12:29 -0700

    remove conditions and add a None arg test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -32,12 +32,7 @@ def text_indentation(text):
         if text[pos] == " " and text[pos - 1] == " ":
             pos += 1
             continue
-        """if text[pos] == " " and text[pos - 1] == "\n":
-            pos += 1
-            continue
-        if text[pos] == " " and text[pos + 1] == "\n":
-            pos += 1
-            continue"""
+
         new_text += text[pos]
         if text[pos] in chars:
             new_text += "\n\n"
