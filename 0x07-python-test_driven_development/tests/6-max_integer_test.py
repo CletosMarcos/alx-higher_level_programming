61447326736ec86b9455e4e1ea858ce0ea62db10
CletosMarcos <cletosmarcos@gmail.com>
2024-05-14 18:21:07 -0700

    add def on methods

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,32 +3,32 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    test_integers(self):
+    def test_integers(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([9, 8, 7, 6]), 9)
 
-    test_floats(self):
+    def test_floats(self):
         self.assertEqual(max_integer([1, 10, 0, 3, 90, 23, 90.01]), 90.01)
         self.assertEqual(max_integer([1.1, 2.5, 5.001, 5.0001]), 5.0001)
 
-    test_ints_and_floats(self):
+    def test_ints_and_floats(self):
         self.assertEqual(maxx_integer([1, 2, 2.1, 6, 9.0, 9.1, 8], 9.1))
 
-    test_no_argument(self):
+    def test_no_argument(self):
         self.assertIsNone(max_integer())
 
 
-    test_mix_values_and_strings(self)
+    def test_mix_values_and_strings(self)
         with self.assertRaises(TypeError):
             max_integer([1, 2, "3"])
 
-    test_pass_a_number(self):
+    def test_pass_a_number(self):
         with assertRaises(TypeError):
             max_integer(1)
 
-    test_undefined_variable(self):
+    def test_undefined_variable(self):
         with assertRaises(NameError):
             max_integer([1, 2, E])
 
-    test_strings(self):
+    def test_strings(self):
         self.assertEqual(max_integer("lsdfg9"))
