e7752479870f7aa301d864389b23b1f830c4c0f3
CletosMarcos <cletosmarcos@gmail.com>
2024-05-14 17:18:17 -0700

    create methods for different tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,15 +1,34 @@
 import unittest
 from 6-max_integer import max_integer
 
-class TestTask_6(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
 
-    test_max_integer(self):
+    test_integers(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([9, 8, 7, 6]), 9)
+
+    test_floats(self):
         self.assertEqual(max_integer([1, 10, 0, 3, 90, 23, 90.01]), 90.01)
-        self.assertEqual(max_integer(None), 0)
+        self.assertEqual(max_integer([1.1, 2.5, 5.001, 5.0001]), 5.0001)
+
+    test_ints_and_floats(self):
+        self.assertEqual(maxx_integer([1, 2, 2.1, 6, 9.0, 9.1, 8], 9.1))
+
+    test_no_argument(self):
+        self.assertIsNone(max_integer())
+
+
+    test_mix_values_and_strings(self)
         with self.assertRaises(TypeError):
             max_integer([1, 2, "3"])
+
+    test_pass_a_number(self):
         with assertRaises(TypeError):
             max_integer(1)
+
+    test_undefined_variable(self):
         with assertRaises(NameError):
             max_integer([1, 2, E])
+
+    test_strings(self):
+        self.assertEqual(max_integer("lsdfg9"))
