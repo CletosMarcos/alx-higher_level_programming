32d640c1bb1873971f2fa2ff0b639c996f0eb33b
CletosMarcos <cletosmarcos@gmail.com>
2024-05-15 20:20:11 -0700

    fix pycodestyle

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -2,6 +2,7 @@
 
 """Module to multiply two matrices"""
 
+
 def matrix_mul(m_a, m_b):
     """ multiplies two provided matrices
 
@@ -15,7 +16,7 @@ def matrix_mul(m_a, m_b):
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
 
-    if  type(m_b) is not list:
+    if type(m_b) is not list:
         raise TypeError("m_b must be a list")
 
     for i in m_a:
@@ -61,7 +62,6 @@ def matrix_mul(m_a, m_b):
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-
     #  Solve matrix mult
 
     inverted_b = []
@@ -81,4 +81,4 @@ def matrix_mul(m_a, m_b):
             new_row.append(prod)
         new_matrix.append(new_row)
 
-    return new_matrix    
+    return new_matrix
