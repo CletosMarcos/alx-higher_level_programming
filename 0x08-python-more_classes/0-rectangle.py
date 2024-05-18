163a7f77ecf7f37fd6aa8d41cd6af69faee97ab5
CletosMarcos <cletosmarcos@gmail.com>
2024-05-17 18:16:37 -0700

    remove line after hashbang

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 """An empty rectangle class"""
 
 
 class Rectangle:
-    """
-    Defines a simple rectangle that does nothing
-    """
+    """ Defines a simple rectangle that does nothing"""
     pass
