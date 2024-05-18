62d14ac977bb3b2903a033928f8b0ce269e6c56e
CletosMarcos <cletosmarcos@gmail.com>
2024-05-17 21:05:35 -0700

    correct typo in __repr__ method

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -57,4 +57,28 @@ class Rectangle:
         return ""
 
     def __repr__(self):
-        return f"Rectangle({self.width}, {self.heigh})"
+        return f"Rectangle({self.width}, {self.height})"
+
+my_rectangle = Rectangle(2, 4)
+print(str(my_rectangle))
+print("--")
+print(my_rectangle)
+print("--")
+print(repr(my_rectangle))
+print("--")
+print(hex(id(my_rectangle)))
+print("--")
+
+# create new instance based on representation
+new_rectangle = eval(repr(my_rectangle))
+print(str(new_rectangle))
+print("--")
+print(new_rectangle)
+print("--")
+print(repr(new_rectangle))
+print("--")
+print(hex(id(new_rectangle)))
+print("--")
+
+print(new_rectangle is my_rectangle)
+print(type(new_rectangle) is type(my_rectangle))
