f3f6025822103a029216eb9c4d9939594803721d
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 20:57:01 -0700

    refactor code | import class from module 7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ module that defines a class BaseGeometry"""
 
+from "7-base_geometry" import BaseGeometry
 
-class BaseGeometry:
+
+class Rectangle(BaseGeometry):
     """
         defines a method that raises an exception
 
@@ -22,26 +24,3 @@ class BaseGeometry:
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-
-    def area(self):
-        """
-            raises an exception
-        """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-            validates value
-
-            args:
-                name: always a string
-                value:
-                    if not an integer: raise a TypeError exception
-                    if less or equal to 0: raise a ValueError exception
-        """
-
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
