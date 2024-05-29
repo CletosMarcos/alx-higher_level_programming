26f8415f156f611e601be5780634ceb8ad8e6635
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 18:16:20 -0700

    fix pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,6 +8,6 @@ def inherits_from(obj, a_class):
         (directly or indirectly) from the specified class ; otherwise False
     """
 
-    return True if (
-            issubclass(type(obj), a_class) and type(obj) != a_class
-            ) else False
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
