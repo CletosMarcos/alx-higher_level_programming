44a6f0a43cba704ea8d43d983086fc0e4d578189
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 21:43:48 -0700

    add task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module that defines a class Square"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+        defines a square
+    """
+
+    def __init__(self, size):
+        """
+            Initializes the super() class with square size
+            using <integer_validator> to validade size
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
