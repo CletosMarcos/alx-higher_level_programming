a0b77d5c2efde7c71ae92effa6aec8cc33b908b7
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 21:09:59 -0700

    add task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" module that defines a class BaseGeometry"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+        defines a method that raises an exception
+
+        methods:
+                area(): raises an exception with the
+                msg "area() is not implemented"
+    """
+
+    def __init__(self, width, height):
+        """
+            Initializes the width and height
+            and making sure the values are valid by using the
+            method <integer_validator>
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+            implementation of the function
+            returns: the rectangle area
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
