388eba0249766ecf2c0bc84321fce89c029ddb4f
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 21:28:47 -0700

    fix private attr on __str__

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -31,4 +31,4 @@ class Square(Rectangle):
         return self.__size ** 2
 
     def __str__(self):
-        return f"[Rectangle] {self._size}/{self.__size}"
+        return f"[Rectangle] {self.__size}/{self.__size}"
