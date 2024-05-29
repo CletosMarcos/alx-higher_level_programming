0bd7955bd4fa87c7f84cf1a68886a22163a165a5
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 20:58:56 -0700

    change import method

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ module that defines a class BaseGeometry"""
 
-from "7-base_geometry" import BaseGeometry
+BaseGeometry = __import("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
