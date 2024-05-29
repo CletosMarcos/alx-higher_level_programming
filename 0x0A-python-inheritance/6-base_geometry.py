a68a0943e8295ea8e73e3334af7434db16a28dcb
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 19:19:13 -0700

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module that defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+        defines a method that raises an exception
+
+        methods:
+                area(): raises an exception with the
+                msg "area() is not implemented"
+    """
+
+    def area(self):
+        raise Exception(""area() is not implemented)
