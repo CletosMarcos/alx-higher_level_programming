6461b75652d2ac8eb2354c79bcc6546ae7528a0e
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 17:53:09 -0700

    correct typo on fuction declaration

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """a module that defines a function inherits_from()"""
 
 
-def inherits_from)(obj, a_class):
+def inherits_from(obj, a_class):
     """
         returns True if the object is an instance of a class that inherited
         (directly or indirectly) from the specified class ; otherwise False
