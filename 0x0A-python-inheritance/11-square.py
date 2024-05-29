68c9191a97c5b4542f9607b46817acbc386a10e0
CletosMarcos <cletosmarcos@gmail.com>
2024-05-28 21:49:44 -0700

    update file 11 and __str__ on file 9

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,6 +13,9 @@ class Square(Rectangle):
         """
             Initializes the super() class with square size
             using <integer_validator> to validade size
+
+            args:
+                size
         """
 
         self.integer_validator("size", size)
