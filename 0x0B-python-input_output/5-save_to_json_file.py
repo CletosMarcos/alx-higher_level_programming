ab973285122833c96a545c4e9defb7b5432b9646
CletosMarcos <cletosmarcos@gmail.com>
2024-05-30 19:35:48 -0700

    fix typo | close ")"

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,4 +14,4 @@ def save_to_json_file(my_obj, filename):
     """
 
    with open(filename, mode="w", encoding="utf-8") as jfile:
-       jfile.write(json.dumps(my_obj)
+       jfile.write(json.dumps(my_obj))
