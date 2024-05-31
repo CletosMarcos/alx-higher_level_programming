1d2d20e91f14e7fae6f1578d41d008db514e8bad
CletosMarcos <cletosmarcos@gmail.com>
2024-05-30 18:30:03 -0700

    add task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a function write_file()"""
+
+
+def write_file(filename="", text=""):
+    """ writes a string to a text file (UTF8) and
+        returns the number of characters written
+        
+        args:
+            filename: file to operate
+            text: the string to write to the file
+    """
+
+    count=0
+    with open(filename, mode="w", encoding="utf-8") as wfile:
+        return wfile.write(text)
