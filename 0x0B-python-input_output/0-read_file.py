8002b1bb0c6a691a27db6ff823f2e1479a34de49
CletosMarcos <cletosmarcos@gmail.com>
2024-05-30 17:49:16 -0700

    add task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function read_file()"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout
+        args:
+            filename: file to be read
+    """
+
+    with open(filename, encoding='utf-8') as nfile:
+        print(nfile.read())
