5167eccbe8ec810c701f9014b0e17555607d3f74
CletosMarcos <cletosmarcos@gmail.com>
2024-05-30 19:24:07 -0700

    add task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a function save_to_json_file()"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation
+
+    args:
+        my_obj: The object to be written to the file
+    """
+
+    json.dump(my_obj, filename)
