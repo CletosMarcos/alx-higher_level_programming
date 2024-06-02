20ce1abeb1803eaa833e88870525b7114cd68a8d
CletosMarcos <cletosmarcos@gmail.com>
2024-06-01 18:05:27 -0700

    review task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,6 +7,5 @@ import json
 def load_from_json_file(filename):
     """ creates an Object from a JSON file """
 
-    with open(filename, encoding="utf-8") as lfile:
-        lfile.read()
-        return load(lfile)
+    with open(filename, encoding='utf-8') as lfile:
+        return json.load(lfile)
