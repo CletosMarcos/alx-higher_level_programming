dbfb06a2bf7f70bb4c329080f3ae850b820bf889
CletosMarcos <cletosmarcos@gmail.com>
2024-06-01 19:22:09 -0700

    try another solution

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,7 +13,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args_list = []
+"""args_list = []
 
 for arg_pos in range(1, len(sys.argv)):
     args_list.append(str(sys.argv[arg_pos]))
@@ -22,4 +22,10 @@ for arg_pos in range(1, len(sys.argv)):
 save_to_json_file(args_list, "add_item.json")
 
 # creates an Object from a JSON file
-load_from_json_file("add_item.json")
+load_from_json_file("add_item.json")"""
+try:
+    items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, "add_item.json")
