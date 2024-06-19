28a320ee829f907a264c4a0311adabc4a14ebcc5
CletosMarcos <cletosmarcos@gmail.com>
2024-06-18 19:10:20 -0700

    add task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+
+            args:
+                attr: If attrs is a list of strings, only attribute names
+                contained in this list must be retrieved.
+
+                Otherwise, all attributes must be retrieved
+            """
+
+        retrieved = {}
+
+        if type(attrs) is list and all(isinstance(pos, str) for pos in attrs):
+            retrieved = {key: self.__dict__[key] for key in attrs
+                         if key in self.__dict__}
+            return retrieved
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
