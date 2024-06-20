85a41e30a31d6166e1a7f4bed6e5c10d012ab695
CletosMarcos <cletosmarcos@gmail.com>
2024-06-19 19:43:22 -0700

    add task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,7 +12,13 @@ def append_after(filename="", search_string="", new_string=""):
             new_string: the string to insert in case the above string is found
     """
 
-    with open(filename, mode='a', encoding='utf-8') as fn:
-        for line in fn:
+    text = ""
+
+    with open(filename, encoding='utf-8') as fr:
+        for line in fr:
+            text += line
             if search_string in line:
-                fn.write(new_string)
+                text += new_string
+    
+    with open(filename, 'w') as fw:
+        fw.write(text)
