f75854595b233fd40f221bc91ac796720d0f6565
CletosMarcos <cletosmarcos@gmail.com>
2024-07-02 17:08:06 -0700

    add documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,6 +3,11 @@
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
+    """Inherits from Rectangle
+
+    __str__ method should return [Square] (<id>) <x>/<y> - <size>
+        where size is width or height"""
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
