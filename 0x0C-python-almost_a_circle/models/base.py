0469e0c968afdb87ba5539273b24a43209d3a76c
CletosMarcos <cletosmarcos@gmail.com>
2024-07-03 17:31:09 -0700

    fix static method from_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -55,6 +55,6 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
-        if not json_string:
-            return "[]"
+        if json_string is None or json_string == "[]":
+            return []
         return json.loads(json_string)
