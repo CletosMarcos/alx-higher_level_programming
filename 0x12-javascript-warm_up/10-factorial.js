7696b43863460be8ac1dd992403c1d4a7b49ac49
CletosMarcos <cletosmarcos@gmail.com>
2024-12-12 15:13:27 -0800

    fix semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ function factoria (value) {
   if (value === 0 || value === 1) {
     return 1;
   }
-  return value *= factoria(value - 1);
+  return value * factoria(value - 1);
 }
 
 const factNr = parseInt(process.argv[2]);
@@ -12,5 +12,5 @@ const factNr = parseInt(process.argv[2]);
 if (factNr) {
   console.log(factoria(factNr));
 } else {
-  console.log(1)
+  console.log(1);
 }
