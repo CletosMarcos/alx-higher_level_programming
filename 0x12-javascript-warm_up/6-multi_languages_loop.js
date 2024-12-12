75cfb036d66514daa956223c14f7589bc620eec0
CletosMarcos <cletosmarcos@gmail.com>
2024-12-11 19:02:44 -0800

    remove " typo on array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const msg = ['C is fun', '“Python is cool', 'JavaScript is amazing'];
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < msg.length; i++) {
   console.log(msg[i]);
