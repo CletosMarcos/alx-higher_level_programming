b23d497b15d62082ac6adac441e981af04a2b566
CletosMarcos <cletosmarcos@gmail.com>
2024-12-13 17:51:20 -0800

    add task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const argList = process.argv;
+
+if (argList.length < 3) {
+  console.log(0);
+} else {
+  const secondBiggest = argList.length - 2;
+
+  for (let i = 2; i < argList.length - 1; i++) {
+    for (let j = 2; j < argList.length - 1; j++) {
+      let aux;
+      if (parseInt(argList[j]) > parseInt(argList[j + 1])) {
+        aux = argList[j];
+        argList[j] = argList[j + 1];
+        argList[j + 1] = aux;
+      }
+    }
+  }
+  console.log(argList[secondBiggest]);
+}
